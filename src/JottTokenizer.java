8e723fed58e430bf19b10840784987c72576958d
Justin Kolodny <jbk2042@g.rit.edu>
2019-10-04 18:23:34 -0400

    Mostly done with Tokenizer, missing cases for concat and charat

--- src/JottTokenizer.java
+++ src/JottTokenizer.java
@@ -7,39 +7,94 @@ public class JottTokenizer {
         ArrayList<String> Tokens = new ArrayList<String>();
         String look="space";
         int spot=0;
+        int end_of_line=0;
+        int print=0;
+        int end_print=0;
+        int comma=0;
         String previous="space";
         for (String token:args) {
-            if(look.equals("quote")){
-
-
+            if(token.charAt(token.length()-1)==';'){
+                end_of_line=1;
+                token=token.substring(0,token.length()-1);
             }
+            if(token.length()-spot>=6) {
+                if (token.charAt(spot) == 'p' && token.charAt(spot + 1) == 'r' && token.charAt(spot + 2) == 'i' &&
+                        token.charAt(spot + 3) == 'n' && token.charAt(spot + 4) == 't' && token.charAt(spot + 5) == '(') {
+                    Tokens.add("print");
+                    print=1;
+                    if(token.length()==6) {
+                        continue;
+                    }
+                    else{
+                        token=token.substring(6);
+                    }
+                }
+            }
+
+            if(print==1){
+                if(token.length()>1) {
+                    if (token.charAt(token.length() - 1) == ')') {
+                        end_print = 1;
+                        print = 0;
+                        token = token.substring(0, token.length() - 1);
+                    }
+                }
+            }
+
             System.out.println(token);
-            look=(tokenizer(token, previous, spot));
-            if(look==null){
-                System.out.println("OH NO");
-                System.exit(0);
+            if (previous.equals("quote")) {
+                while (look.equals("quote")) {
+                    if (spot >= token.length()) {
+                        previous = "quote";
+                        break;
+                    }
+                    look = tokenizer(token, previous, spot);
+                    spot += 1;
+                    if (look.equals("string")) {
+                        Tokens.add(look);
+                        previous = "space";
+                    }
+                }
             }
-            if(look.equals("quote")){
-                previous="quote";
-                spot+=1;
-                look=tokenizer(token, previous, spot);
-                if(look==null){
+            else {
+                look = (tokenizer(token, previous, spot));
+                if (look == null) {
                     System.out.println("OH NO");
                     System.exit(0);
                 }
-                while(look.equals("quote")){
-                    spot+=1;
-                    if(spot==token.length()){
-                        break;
+                if (look.equals("quote")) {
+                    previous = "quote";
+                    spot += 1;
+                    look = tokenizer(token, previous, spot);
+                    if (look == null) {
+                        System.out.println("OH NO");
+                        System.exit(0);
                     }
-                    look=tokenizer(token, previous, spot);
-                    if(look.equals("string")){
-                        Tokens.add(look);
-                        previous="space";
+                    while (look.equals("quote")) {
+                        spot += 1;
+                        if (spot == token.length()) {
+                            previous = "quote";
+                            break;
+                        }
+                        look = tokenizer(token, previous, spot);
+                        if (look.equals("string")) {
+                            Tokens.add(look);
+                            previous = "space";
+                        }
                     }
+                } else {
+                    Tokens.add(look);
                 }
             }
-            spot=0;
+            spot = 0;
+            if(end_print==1){
+                Tokens.add("end_parenthesis");
+                end_print=0;
+            }
+            if(end_of_line==1){
+                Tokens.add("end_stmt");
+                end_of_line=0;
+            }
         }
         for (String thing: Tokens) {
             System.out.println(thing);
@@ -48,6 +103,27 @@ public class JottTokenizer {
 
     private static String tokenizer(String token, String previous, int spot) {
         if(previous.equals("space")) {
+            if(token.length()==6) {
+                if (token.charAt(0) == 'S' && token.charAt(1) == 't' && token.charAt(2) == 'r'
+                        && token.charAt(3) == 'i' && token.charAt(4) == 'n' && token.charAt(5) == 'g') {
+                    return "type_String";
+                }
+                if (token.charAt(0) == 'D' && token.charAt(1) == 'o' && token.charAt(2) == 'u'
+                        && token.charAt(3) == 'b' && token.charAt(4) == 'l' && token.charAt(5) == 'e') {
+                    return "type_Double";
+                }
+                if (token.charAt(spot) == 'p' && token.charAt(spot + 1) == 'r' && token.charAt(spot + 2) == 'i' &&
+                        token.charAt(spot + 3) == 'n' && token.charAt(spot + 4) == 't' && token.charAt(spot + 5) == '(') {
+                    return "print";
+                }
+            }
+            if(token.length()==7) {
+                if (token.charAt(0) == 'I' && token.charAt(1) == 'n' && token.charAt(2) == 't'
+                        && token.charAt(3) == 'e' && token.charAt(4) == 'g' && token.charAt(5) == 'e'
+                        && token.charAt(4) == 'r') {
+                    return "type_Integer";
+                }
+            }
             if (token.charAt(spot) == '+' && token.length()==1) {
                 return "plus";
             }
@@ -90,12 +166,15 @@ public class JottTokenizer {
                 return tokenizer(token, "period", spot+1);
             }
             if ((token.charAt(spot) >= 97 && token.charAt(spot) <= 122)||(token.charAt(spot)>=65 && token.charAt(spot)<=90)) {
-                return tokenizer(token, "keyword", spot+1);
+                if (token.length() == spot + 1) {
+                    return "keyword";
+                } else {
+                    return tokenizer(token, "keyword", spot + 1);
+                }
             }
             if (token.charAt(spot) == '"') {
-                return "quote";
+                return tokenizer(token, "quote", spot+1);
             }
-            return null;
         }
         if(previous.equals("number")){
             if(token.length()==spot+1) {
@@ -111,9 +190,9 @@ public class JottTokenizer {
         if(previous.equals("period")){
             if(token.charAt(spot) >= 48 && token.charAt(spot) <= 57){
                 if(token.length()==spot+1){
-                    return "number";
+                    return "double";
                 }
-                return tokenizer(token, "number", spot+1);
+                return tokenizer(token, "period", spot+1);
             }
         }
         if(previous.equals("quote")){
