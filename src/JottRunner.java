c85fffb2864504d9bb571569d072e4de31700c01
Eric Lang <Eric@mail.rit.edu>
2019-10-06 20:09:27 -0400

    JottRunner reads file, and will send lines that aren't comments to the Tokenizer.

--- src/JottRunner.java
+++ src/JottRunner.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class JottRunner {
@@ -11,16 +10,23 @@ public class JottRunner {
 
             File code = new File(args[0]); //Java:java Jott program.j eg: args[0] --> program.j
             Scanner s = new Scanner(code);
-            while(s.hasNextLine()) {
+            while (s.hasNextLine()) {
                 /*processing of each line happens here*/
-                System.out.println(s.nextLine());
+                String nextLine = s.nextLine();
+                if (nextLine.replace(" ", "").substring(0, 2) != "//") { //remove whitespace from string, and only send to tokenizer if it isn't a comment.
+                    replace_this_with_what_sends_the_line_to_the_tokenizer(nextLine);
+                } else {  //print a string about receiving a comment; but really, else do nothing, because it must've been a comment, which we don't need.
+                    System.out.println("+++++++++   Comment Received: " + nextLine);
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("Error, invalid file.");
         }
+    }
 
-
+    static void replace_this_with_what_sends_the_line_to_the_tokenizer(String line) {
+        System.out.println("+++++++++   Code sent to tokenizer: " + line);
     }
 
 }
--- src/test.j
+++ src/test.j
@@ -1,2 +1,6 @@
 print("hello")
+//this is a comment.
+//this is a comment.
+   //this is also a comment.
+   //hi justin.
 print("goodbye")
