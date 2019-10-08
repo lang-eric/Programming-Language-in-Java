12cec783796a78147cc9fc5a8e4d1282182cc2eb
Jasmine <973193460@qq.com>
2019-10-08 19:47:03 -0400

    Created and Finished Grammar class for Jott

--- src/Grammar.java
+++ src/Grammar.java
@@ -1,15 +1,27 @@
 package src;
 
+import org.w3c.dom.Node;
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *  A Grammar class that represents the grammar of Jott.
+ *  The non-terminal is a parent node, and the child node of its next level
+ *  is stored in an ArrayList.
+ *
+ *  For example, <stmt> --> <print> | <asmt> | <expr><end_statement>
+ *              parent node : stmt
+ *              child nodes: [[print], [asmt], [expr, end_statement]]
+ */
 public class Grammar {
-    public List<List<Type>> programLR(){
 
-        List<List<Type>> lst = new ArrayList<>();
-        List<Type> list = new ArrayList<>();
-        list.add(Type.stmt_list);
-        list.add(Type.ddollar);
+    public List<List<NodeType>> programLR(){
+
+        List<List<NodeType>> lst = new ArrayList<>();
+        List<NodeType> list = new ArrayList<>();
+        list.add(NodeType.STMT_LIST);
+        list.add(NodeType.DDOLLAR);
 
         lst.add(list);
 
@@ -17,14 +29,14 @@ public class Grammar {
     }
 
 
-    public List<List<Type>> stmtListLR(){
-        List<List<Type>> lst = new ArrayList<>();
-        List<Type> lst1 = new ArrayList<>();
-        lst1.add(Type.stmt);
-        lst1.add(Type.stmt_list);
+    public List<List<NodeType>> stmtListLR(){
+        List<List<NodeType>> lst = new ArrayList<>();
+        List<NodeType> lst1 = new ArrayList<>();
+        lst1.add(NodeType.STMT);
+        lst1.add(NodeType.STMT_LIST);
 
-        List<Type> lst2 = new ArrayList<>();
-        lst2.add(Type.empty);
+        List<NodeType> lst2 = new ArrayList<>();
+        lst2.add(NodeType.EMPTY);
 
         lst.add(lst1);
         lst.add(lst2);
@@ -33,30 +45,30 @@ public class Grammar {
     }
 
 
-    public List<Type> startParenLR(){
-        List<Type> lst = new ArrayList<>();
-        lst.add(Type.start_paren);
+    public List<NodeType> startParenLR(){
+        List<NodeType> lst = new ArrayList<>();
+        lst.add(NodeType.START_PAREN);
         return lst;
     }
 
-    public List<Type> endParenLR(){
-        List<Type> lst = new ArrayList<>();
-        lst.add(Type.end_paren);
+    public List<NodeType> endParenLR(){
+        List<NodeType> lst = new ArrayList<>();
+        lst.add(NodeType.END_PAREN);
         return lst;
     }
 
 
-    public List<Type> endStmtLR(){
-        List<Type> lst = new ArrayList<>();
-        lst.add(Type.end_stmt);
+    public List<NodeType> endStmtLR(){
+        List<NodeType> lst = new ArrayList<>();
+        lst.add(NodeType.END_STMT);
         return lst;
     }
 
-    public List<Type> echarLR(){
-        List<Type> lst = new ArrayList<>();
-        lst.add(Type.l_char);
-        lst.add(Type.u_char);
-        lst.add(Type.digit);
+    public List<NodeType> echarLR(){
+        List<NodeType> lst = new ArrayList<>();
+        lst.add(NodeType.L_CHAR);
+        lst.add(NodeType.U_CHAR);
+        lst.add(NodeType.DIGIT);
         return lst;
     }
 
@@ -88,36 +100,37 @@ public class Grammar {
         return lst;
     }
 
-    public List<Type> signLR(){
-        List<Type> lst = new ArrayList<>();
-        lst.add(Type.plus);
-        lst.add(Type.minus);
-        lst.add(Type.empty);
+    public List<NodeType> signLR(){
+        List<NodeType> lst = new ArrayList<>();
+        lst.add(NodeType.PLUS);
+        lst.add(NodeType.MINUS);
+        lst.add(NodeType.EMPTY);
 
         return lst;
     }
 
-    public List<List<Type>> idLR(){
-        List<List<Type>> lst = new ArrayList<>();
-        List<Type> lst1 = new ArrayList<>();
-        lst1.add(Type.l_char);
-        lst1.add(Type.CHAR);
+    public List<List<NodeType>> idLR(){
+        List<List<NodeType>> lst = new ArrayList<>();
+        List<NodeType> lst1 = new ArrayList<>();
+        lst1.add(NodeType.L_CHAR);
+        lst1.add(NodeType.CHAR);
         lst.add(lst1);
 
         return lst;
     }
 
-    public List<List<Type>> stmtLR(){
-        List<List<Type>> lst = new ArrayList<>();
-        List<Type> lst1 = new ArrayList<>();
-        lst1.add(Type.expr);
-        lst1.add(Type.end_stmt);
+    public List<List<NodeType>> stmtLR(){
+        List<List<NodeType>> lst = new ArrayList<>();
+        List<NodeType> lst1 = new ArrayList<>();
+        lst1.add(NodeType.EXPR);
+        lst1.add(NodeType.END_STMT);
 
-        List<Type> lst2 = new ArrayList<>();
-        lst2.add(Type.print);
+        List<NodeType> lst2 = new ArrayList<>();
+        lst2.add(NodeType.PRINT);
 
-        List<Type> lst3 = new ArrayList<>();
-        lst3.add(Type.asmt);
+        List<NodeType> lst3 = new ArrayList<>();
+        lst3.add(NodeType.ASMT);
+
 
         lst.add(lst1);
         lst.add(lst2);
@@ -126,42 +139,43 @@ public class Grammar {
         return lst;
     }
 
-    public List<Type> exprLR(){
-        List<Type> lst = new ArrayList<>();
-        lst.add(Type.i_expr);
-        lst.add(Type.d_expr);
-        lst.add(Type.s_expr);
-        lst.add(Type.id);
+    public List<NodeType> exprLR(){
+        List<NodeType> lst = new ArrayList<>();
+        lst.add(NodeType.I_EXPR);
+        lst.add(NodeType.D_EXPR);
+        lst.add(NodeType.S_EXPR);
+        lst.add(NodeType.ID);
 
         return lst;
     }
 
-    public List<List<Type>> printLR(){
-        List<List<Type>> lst = new ArrayList<>();
-        List<Type> list = new ArrayList<>();
-        list.add(Type.start_paren);
-        list.add(Type.expr);
-        list.add(Type.end_paren);
-        list.add(Type.end_stmt);
+    public List<List<NodeType>> printLR(){
+        List<List<NodeType>> lst = new ArrayList<>();
+        List<NodeType> list = new ArrayList<>();
+        list.add(NodeType.START_PAREN);
+        list.add(NodeType.EXPR);
+        list.add(NodeType.END_PAREN);
+        list.add(NodeType.END_STMT);
         lst.add(list);
 
         return lst;
     }
 
-    public List<List<Type>> asmtLR(){
-        List<List<Type>> lst = new ArrayList<>();
+    public List<List<NodeType>> asmtLR(){
+        List<List<NodeType>> lst = new ArrayList<>();
 
-        List<Type> lst1 = new ArrayList<>();
-        lst1.add(Type.d_expr);
-        lst1.add(Type.end_stmt);
+        List<NodeType> lst1 = new ArrayList<>();
+        lst1.add(NodeType.D_EXPR);
+        lst1.add(NodeType.END_STMT);
 
-        List<Type> lst2 = new ArrayList<>();
-        lst2.add(Type.i_expr);
-        lst2.add(Type.end_stmt);
+        List<NodeType> lst2 = new ArrayList<>();
+        lst2.add(NodeType.I_EXPR);
+        lst2.add(NodeType.END_STMT);
 
-        List<Type> lst3 = new ArrayList<>();
-        lst3.add(Type.s_expr);
-        lst3.add(Type.end_stmt);
+        List<NodeType> lst3 = new ArrayList<>();
+        lst3.add(NodeType.S_EXPR);
+        lst3.add(NodeType.END_STMT);
+
 
         lst.add(lst1);
         lst.add(lst2);
@@ -172,16 +186,162 @@ public class Grammar {
 
 
 
-    public List<Type> opLR(){
-        List<Type> lst = new ArrayList<>();
-        lst.add(Type.plus);
-        lst.add(Type.minus);
-        lst.add(Type.mult);
-        lst.add(Type.div);
-        lst.add(Type.power);
+    public List<NodeType> opLR(){
+        List<NodeType> lst = new ArrayList<>();
+        lst.add(NodeType.PLUS);
+        lst.add(NodeType.MINUS);
+        lst.add(NodeType.MULT);
+        lst.add(NodeType.DIV);
+        lst.add(NodeType.POWER);
 
         return lst;
     }
+
+    public List<List<NodeType>> dblLR(){
+        List<List<NodeType>> lst = new ArrayList<>();
+
+        List<NodeType> lst1 = new ArrayList<>();
+        lst1.add(NodeType.SIGN);
+        lst1.add(NodeType.DOT);
+        lst1.add(NodeType.DIGIT);
+
+        lst.add(lst1);
+
+        return lst;
+    }
+
+    public List<List<NodeType>> dExprLR(){
+        List<List<NodeType>> lst = new ArrayList<>();
+
+        List<NodeType> lst1 = new ArrayList<>();
+        lst1.add(NodeType.ID);
+
+        List<NodeType> lst2 = new ArrayList<>();
+        lst2.add(NodeType.DBL);
+
+        List<NodeType> lst3 = new ArrayList<>();
+        lst3.add(NodeType.DBL);
+        lst3.add(NodeType.OP);
+        lst3.add(NodeType.DBL);
+
+        List<NodeType> lst4 = new ArrayList<>();
+        lst4.add(NodeType.DBL);
+        lst4.add(NodeType.OP);
+        lst4.add(NodeType.D_EXPR);
+
+        List<NodeType> lst5 = new ArrayList<>();
+        lst5.add(NodeType.D_EXPR);
+        lst5.add(NodeType.OP);
+        lst5.add(NodeType.DBL);
+
+        List<NodeType> lst6 = new ArrayList<>();
+        lst6.add(NodeType.D_EXPR);
+        lst6.add(NodeType.OP);
+        lst6.add(NodeType.D_EXPR);
+
+
+        lst.add(lst1);
+        lst.add(lst2);
+        lst.add(lst3);
+        lst.add(lst4);
+        lst.add(lst5);
+        lst.add(lst6);
+
+        return lst;
+    }
+
+    public List<List<NodeType>> intLR(){
+        List<List<NodeType>> lst = new ArrayList<>();
+
+        List<NodeType> lst1 = new ArrayList<>();
+        lst1.add(NodeType.SIGN);
+        lst1.add(NodeType.DIGIT);
+
+        lst.add(lst1);
+
+        return lst;
+    }
+
+    public List<List<NodeType>> iExprLR(){
+        List<List<NodeType>> lst = new ArrayList<>();
+
+        List<NodeType> lst1 = new ArrayList<>();
+        lst1.add(NodeType.ID);
+
+        List<NodeType> lst2 = new ArrayList<>();
+        lst2.add(NodeType.INT);
+
+        List<NodeType> lst3 = new ArrayList<>();
+        lst3.add(NodeType.INT);
+        lst3.add(NodeType.OP);
+        lst3.add(NodeType.INT);
+
+        List<NodeType> lst4 = new ArrayList<>();
+        lst4.add(NodeType.INT);
+        lst4.add(NodeType.OP);
+        lst4.add(NodeType.I_EXPR);
+
+        List<NodeType> lst5 = new ArrayList<>();
+        lst5.add(NodeType.I_EXPR);
+        lst5.add(NodeType.OP);
+        lst5.add(NodeType.INT);
+
+        List<NodeType> lst6 = new ArrayList<>();
+        lst6.add(NodeType.I_EXPR);
+        lst6.add(NodeType.OP);
+        lst6.add(NodeType.I_EXPR);
+
+
+        lst.add(lst1);
+        lst.add(lst2);
+        lst.add(lst3);
+        lst.add(lst4);
+        lst.add(lst5);
+        lst.add(lst6);
+
+        return lst;
+    }
+
+    public List<List<NodeType>> sExprLR(){
+        List<List<NodeType>> lst = new ArrayList<>();
+
+        List<NodeType> lst1 = new ArrayList<>();
+        lst1.add(NodeType.ID);
+
+        List<NodeType> lst2 = new ArrayList<>();
+        lst2.add(NodeType.STR_LITERAL);
+
+        List<NodeType> lst3 = new ArrayList<>();
+        lst3.add(NodeType.CONCAT);
+        lst3.add(NodeType.START_PAREN);
+        lst3.add(NodeType.S_EXPR);
+        lst3.add(NodeType.COMMA);
+        lst3.add(NodeType.S_EXPR);
+        lst3.add(NodeType.END_PAREN);
+
+        List<NodeType> lst4 = new ArrayList<>();
+        lst4.add(NodeType.CHARAT);
+        lst4.add(NodeType.START_PAREN);
+        lst4.add(NodeType.S_EXPR);
+        lst4.add(NodeType.COMMA);
+        lst4.add(NodeType.I_EXPR);
+        lst4.add(NodeType.END_PAREN);
+
+        lst.add(lst1);
+        lst.add(lst2);
+        lst.add(lst3);
+        lst.add(lst4);
+
+        return lst;
+    }
+
+    public List<NodeType> strLR(){
+        List<NodeType> lst = new ArrayList<>();
+        lst.add(NodeType.STR);
+        return lst;
+    }
+
+
 
 
 }
