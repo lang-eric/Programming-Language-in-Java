f070da14ba5c28bac0ff5d9d510abbea2a151a30
Jasmine <973193460@qq.com>
2019-10-08 19:48:06 -0400

    Updated Parser classes

--- src/JottParser.java
+++ src/JottParser.java
@@ -4,13 +4,12 @@ import src.ParseTreeNode;
 
 import java.util.List;
 
-public class JottParser {
-	public static ParseTreeNode parseTokens(List<String> tokenList, List<Type> types) {
-		// x = 1 + 3
-		// print('hello')
-		// print(3 + 5)
+import java.util.List;
 
-		ParseTreeNode root = new ParseTreeNode(null);
+public class JottParser {
+	public static ParseTreeNode parseTokens(List<String> tokenList) {
+		ParseTreeNode root = new ParseTreeNode(null, ParseTreeNode.NodeType.PROGRAM);
+		root.addChild(new ParseTreeNode(root, ParseTreeNode.NodeType.STMT_LIST));
 
 
 		return null; // TODO: make something you can return that isn't NULL
