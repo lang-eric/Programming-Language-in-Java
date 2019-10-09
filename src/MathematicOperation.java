c5b15fa4e9ec196900f373c9efd3d61cd623ff4f
Justin Kolodny <jbk2042@g.rit.edu>
2019-10-08 21:48:31 -0400

    Fixed some bugs in JottTokenizer. Made the divide by zero exception in MathematicOperation such that it will crash the program and it throws a more appropriate message.

--- src/MathematicOperation.java
+++ src/MathematicOperation.java
@@ -43,7 +43,8 @@ public class MathematicOperation {
 
                 }
                 catch (Exception e){
-                    System.out.println("zero can not be divided...");
+                    System.out.println("Runtime Error: Cannot divide by zero!");
+                    System.exit(-1);
                 }
             }
 
